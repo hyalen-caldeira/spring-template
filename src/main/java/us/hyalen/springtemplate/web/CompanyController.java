55cdff7c5ac69d931853f6fb1e89181578f856ee
Hyalen Caldeira <hyalen.moreira@gmail.com>
2022-01-18 00:13:31 -0500

    Created an exception class

--- src/main/java/us/hyalen/springtemplate/web/CompanyController.java
+++ src/main/java/us/hyalen/springtemplate/web/CompanyController.java
@@ -3,13 +3,23 @@ package us.hyalen.springtemplate.web;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.ResponseEntity.created;
+import static org.springframework.http.ResponseEntity.ok;
+
+import us.hyalen.springtemplate.dto.CompanyDto;
+import us.hyalen.springtemplate.exception.NotFoundException;
 import us.hyalen.springtemplate.model.CompanyModel;
 import us.hyalen.springtemplate.service.CompanyService;
 
 import java.util.List;
+import java.util.Optional;
 
-@RestController
+@RestController("companyController_v1")
+@RequestMapping(value = "/api/companies", produces = CompanyDto.MEDIA_TYPE)
 public class CompanyController {
     private CompanyService service;
 
@@ -17,7 +27,15 @@ public class CompanyController {
         this.service = service;
     }
 
-    @GetMapping("/company")
+    @GetMapping(value = "/{name}")
+    public ResponseEntity<CompanyDto> getCompanyByName(@PathVariable(value = "name") String name) {
+//        Optional<CompanyDto> companyDto = service.findByName(name).orElseThrow(NotFoundException::new);
+        CompanyDto companyDto = service.findByName(name).orElseThrow(NotFoundException::new);
+
+        return ok(companyDto);
+    }
+
+    @GetMapping
     public ResponseEntity<List<CompanyModel>> getAllCompanies() {
         var list = service.getAllCompanies();
         return new ResponseEntity<>(list, HttpStatus.OK);
